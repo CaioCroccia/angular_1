9092d13f72385b767b569805ac6205e388b75f7d
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-25 22:55:24 -0300

    1 file modified, 4 added

---
+++ src/app/juro-simples-component/juro-simples-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juro-simples-component',
+  templateUrl: './juro-simples-component.component.html',
+  styleUrls: ['./juro-simples-component.component.css']
+})
+export class JuroSimplesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/juros-composto-component/juros-composto-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros-composto-component',
+  templateUrl: './juros-composto-component.component.html',
+  styleUrls: ['./juros-composto-component.component.css']
+})
+export class JurosCompostoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
