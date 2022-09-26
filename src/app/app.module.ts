01c52c0ed5ec459ea0b4bdcf8830d81e5fbf9892
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-09-25 23:59:23 -0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { JuroSimplesComponentComponent } from './juro-simples-component/juro-simples-component.component';
+import { JurosCompostoComponentComponent } from './juros-composto-component/juros-composto-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, JuroSimplesComponentComponent, JurosCompostoComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
